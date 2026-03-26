e369f7b306668ad5581147e893c078872db45e2d
Christian <christiancarrascoso.22@gmail.com>
2026-03-26 13:35:42 +0100

    invertir los colores y poner los escudos

--- semanasantadoshermanas/scripts/script.js
+++ semanasantadoshermanas/scripts/script.js
@@ -1,7 +1,10 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menuNav = document.querySelector('.menu-nav');
+const menuBtn = document.querySelector(".menu-btn");
+const menuNav = document.querySelector(".menu-nav");
 
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener("click", () => {
+  /*
     menuNav.style.display = menuNav.style.display === 'none' ? 'block' : 'none';
     menuBtn.querySelector('.menu-btn-icon').textContent = menuNav.style.display === 'none' ? '☰' : '✖';
+    */
+  menuNav.classList.toggle("active");
 });
